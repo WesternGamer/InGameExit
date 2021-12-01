b967e987dafc8a000d5e5aeba93583f84e4aa20c
WesternGamer <80211714+WesternGamer@users.noreply.github.com>
2021-12-01 17:59:36 -0500

    Update AssemblyInfo.cs

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,15 +1,15 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SEPluginTemplate")]
+[assembly: AssemblyTitle("InGameExit")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("WesternGamer")]
-[assembly: AssemblyProduct("SEPluginTemplate")]
+[assembly: AssemblyProduct("InGameExit")]
 [assembly: AssemblyCopyright("Copyright © WesternGamer 2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
